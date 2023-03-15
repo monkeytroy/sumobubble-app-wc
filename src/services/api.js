0bcfe17872a66ea4cb5c4b7f28216550b9904fd0
Fred Arters <fredarters@gmail.com>
2023-03-15 01:24:42 -0400

    Change to regex

--- src/services/api.js
+++ src/services/api.js
@@ -4,16 +4,17 @@ let serviceBase = '';
 
 export const getConfig = async (customer) => {
   try {
+    
     const src = document.querySelector('#beacon-app-scriptastic')?.src;
+    serviceBase = src.match(/http[s]?:\/\/.+?\//gm)[0];
 
-    serviceBase = src.split('/').slice(0, -1).join('/') + '/';
-
-    const res = await fetch(`${serviceBase}${customer}.json`);
+    const res = await fetch(`${serviceBase}app/${customer}.json`);
     const data = await res.json();
     config = data;
+
     return data;
   } catch(err) {
-    console.error('Could not load the Beacon App script.  Check your config');
+    console.error('Could not load the Beacon App script.  Check your config.', err);
     return null;
   }
 }
@@ -25,7 +26,7 @@ export const sendContact = async ({email, name, message, token}) => {
   if (token && serviceBase) {
 
     // call contact with token
-    const res = await fetch(`${serviceBase}/api/contact`, {
+    const res = await fetch(`${serviceBase}api/contact`, {
       method: 'POST',
       body: JSON.stringify({
         emailTo,
