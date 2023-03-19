40827b8d9ebe747f4c13171ece24224b145fca3c
Fred Arters <fredarters@gmail.com>
2023-03-18 20:04:11 -0400

    Fix overflow and title spacing

--- src/components/BeaconButton.vue
+++ src/components/BeaconButton.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="fixed bottom-6 right-6 z-50">
+
     <img src="../assets/i-icon.png" 
       class="w-12 h-12 select-none
       cursor-pointer shadow-md shadow-gray-200
@@ -12,40 +13,43 @@
       :class="open ? 'visible': 'hidden'">
       
       <div class="w-screen max-w-md rounded-3xl bg-white
-        text-sm shadow-lg ring-1 ring-gray-900/5 p-2 flex"
+        text-sm shadow-lg ring-1 ring-gray-900/5 flex flex-col"
         style="max-height: 90vh !important">
 
-          <div class="rounded-t-3xl absolute inset-x-0 top-0 p-4 
-            text-gray-800 bg-skin-primary bg-opacity-40
-            text-2xl font-bold select-none
-            flex justify-between items-center uppercase z-20">
-            {{ config?.customer?.title }}
-            <XCircleIcon class="w-6 h-6 cursor-pointer hover:text-gray-600" 
-              @click="close">
-            </XCircleIcon>
+          <div class="rounded-t-3xl 
+            bg-skin-primary bg-opacity-40
+            text-2xl font-bold select-none">
+            <div class="flex justify-between items-center uppercase p-4 text-gray-800">
+              {{ config?.customer?.title }}
+              <XCircleIcon class="w-6 h-6 cursor-pointer hover:text-gray-600" 
+                @click="close">
+              </XCircleIcon>
+            </div>
           </div>
 
-          <div class="overflow-y-auto px-2 mt-16">
-            <ul role="list" class="space-y-2">
-              <li class="relative">
-                <SummaryPanel :config="config"></SummaryPanel>
-              </li>
-              <li class="relative">
-                <SpecialPanel :config="config"></SpecialPanel>
-              </li>
-              <li class="relative">
-                <ContactPanel :config="config"></ContactPanel>
-              </li>
-              <li class="relative">
-                <VersePanel :config="config"></VersePanel>
-              </li>
-              <li class="relative">
-                <VodPanel :config="config"></VodPanel>
-              </li>
-              <li class="relative">
-                <FunnyPanel :config="config"></FunnyPanel>
-              </li>
-            </ul>
+          <div class="p-2 grow rounded-b-3xl overflow-hidden flex flex-col">
+            <div class="overflow-y-auto p-2">
+              <ul role="list" class="space-y-2">
+                <li class="relative">
+                  <SummaryPanel :config="config"></SummaryPanel>
+                </li>
+                <li class="relative">
+                  <SpecialPanel :config="config"></SpecialPanel>
+                </li>
+                <li class="relative">
+                  <ContactPanel :config="config"></ContactPanel>
+                </li>
+                <li class="relative">
+                  <VersePanel :config="config"></VersePanel>
+                </li>
+                <li class="relative">
+                  <VodPanel :config="config"></VodPanel>
+                </li>
+                <li class="relative">
+                  <FunnyPanel :config="config"></FunnyPanel>
+                </li>
+              </ul>
+            </div>
           </div>
 
       </div>
