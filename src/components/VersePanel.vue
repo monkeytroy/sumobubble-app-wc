39dc4b7136d1f15680b73df38277236fc1bb0918
Fred Arters <fredarters@gmail.com>
2023-04-20 01:53:24 -0400

    Update copyright display

--- src/components/VersePanel.vue
+++ src/components/VersePanel.vue
@@ -1,12 +1,20 @@
 <template>
   <AccordianContent title="Daily Verse" v-if="verse.enabled" :config="config" scrollItem="versePanelRef">
 
-    <div v-if="verse.props.verseRef" class="text-xl font-bold text-gray-800">{{ verse.props.verseRef }}</div>
+    <div v-if="verse.props.verseRef" class="text-xl font-bold text-gray-800 flex gap-2 align-top">
+      {{ verse.props.verseRef }}
+      <Popover v-if="verse.props?.copyright" v-slot="{ open }" class="font-normal text-sm" >
+        <PopoverButton>
+          (c)
+        </PopoverButton>
+
+        <PopoverPanel class="absolute left-0 p-2 bg-white border-gray-500 border-2 rounded-md shadow-lg">
+          <div class="text-xs text-gray-500 my-2">{{ verse.props?.copyright }}</div>    
+        </PopoverPanel>
+      </Popover>
+    </div>
 
     <Markdown :source="verse.content || ''" class="text-xl text-gray-800"></Markdown>
-
-    <div v-if="verse.props?.copyright" 
-      class="text-xs text-gray-500 my-2">{{ verse.props?.copyright }}</div>
       
   </AccordianContent>
 </template>
@@ -15,7 +23,8 @@
 
   import { computed, defineProps } from 'vue';
   import AccordianContent from '@/components//AccordionContent.vue';
-  import Markdown from '@/components/Markdown.vue'
+  import Markdown from '@/components/Markdown.vue';
+  import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
   
   const props = defineProps(['config']);
 
