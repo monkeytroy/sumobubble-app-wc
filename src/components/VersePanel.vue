41f88622eef105c24684cec6421a83653d335990
Fred Arters <fredarters@gmail.com>
2023-04-20 02:16:05 -0400

    Fix c popover

--- src/components/VersePanel.vue
+++ src/components/VersePanel.vue
@@ -1,17 +1,20 @@
 <template>
   <AccordianContent title="Daily Verse" v-if="verse.enabled" :config="config" scrollItem="versePanelRef">
 
-    <div v-if="verse.props.verseRef" class="text-xl font-bold text-gray-800 flex gap-2 align-top">
+    <div v-if="verse.props.verseRef" class="text-xl font-bold text-gray-800 flex gap-2 items-center relative">
       {{ verse.props.verseRef }}
-      <Popover v-if="verse.props?.copyright" v-slot="{ open }" class="font-normal text-sm" >
-        <PopoverButton>
+      <div v-if="verse.props?.copyright" class="font-normal text-sm cursor-pointer"
+        @click="showCopyright = !showCopyright">
           (c)
-        </PopoverButton>
-
-        <PopoverPanel class="absolute left-0 p-2 bg-white border-gray-500 border-2 rounded-md shadow-lg">
-          <div class="text-xs text-gray-500 my-2">{{ verse.props?.copyright }}</div>    
-        </PopoverPanel>
-      </Popover>
+      </div>
+      <div v-if="showCopyright" 
+        @mouseout="showCopyright = false" 
+        @click="showCopyright = false"
+        class="absolute left-0 m-6">
+        <div class="bg-white border-gray-500 border-2 rounded-md shadow-lg cursor-pointer">
+          <div class="text-xs text-gray-500 p-4">{{ verse.props?.copyright }}</div>    
+        </div>
+      </div>
     </div>
 
     <Markdown :source="verse.content || ''" class="text-xl text-gray-800"></Markdown>
@@ -21,12 +24,12 @@
 
 <script lang="ts" setup>
 
-  import { computed, defineProps } from 'vue';
+  import { computed, defineProps, ref } from 'vue';
   import AccordianContent from '@/components//AccordionContent.vue';
   import Markdown from '@/components/Markdown.vue';
-  import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
   
   const props = defineProps(['config']);
+  const showCopyright = ref(false);
 
   const verse = computed(() => props?.config?.sections?.verse || {});
 
