ca7e4b92befe371688ee3cec8a5afc8cb4c86eeb
Fred Arters <fredarters@gmail.com>
2023-04-20 10:22:55 -0400

    Pop up up

--- src/components/CatSelect.vue
+++ src/components/CatSelect.vue
@@ -16,7 +16,7 @@
         leave-from-class="opacity-100" 
         leave-to-class="opacity-0">
         
-        <ListboxOptions class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md 
+        <ListboxOptions class="absolute bottom-full z-10 mt-1 max-h-60 w-full overflow-auto rounded-md 
           bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
           <ListboxOption as="template" v-for="(cat, index) in categories" 
             :key="index" :value="cat?.title" v-slot="{ active, selected }">
