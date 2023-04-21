bc235beba6048125c935eb4e563512683a6d7b52
Fred Arters <fredarters@gmail.com>
2023-04-20 20:49:31 -0400

    Fix logo container

--- src/components/SummaryPanel.vue
+++ src/components/SummaryPanel.vue
@@ -1,15 +1,12 @@
 <template>
-  <div class="flex-col" v-if="content">
-    
-    <div class="flex select-none mb-4" 
-      :class="isLogoCenter ? 'justify-center': 'justify-start'">
-      <img :src="config?.customer?.logo?.url" class="max-w-full max-h-24"/>
+  <div class="flex-col select-none">
+    <div id="summaryContent" class="fred text-lg" 
+      v-if="content"
+      v-html="safeHTML(content)">
+    </div>  
+    <div v-else class="fred text-lg">
+      Welcome! Glad you could stop by.
     </div>
-
-    <div v-if="content">
-      <div id="summaryContent" class="fred text-lg" v-html="safeHTML(content)"></div>
-    </div>
-    
     <div class=""></div>
   </div>
 </template>
@@ -24,7 +21,5 @@
   const safeHTML = VueSecureHTML.safeHTML;
   
   const content = computed(() => props.config?.summary?.content || '');
-
-  const isLogoCenter = computed( e => props.config?.customer?.logo?.align == 'center');
 
 </script>
